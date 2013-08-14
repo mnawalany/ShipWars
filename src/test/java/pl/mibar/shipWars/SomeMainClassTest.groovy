d393634b6cbc35529854416a36e889a9c792f57d
mnawalany <m.nawalany@gmail.com>
2013-08-14 22:31:57 +0100

    Test

---
+++ src/test/java/pl/mibar/shipWars/SomeMainClassTest.groovy
@@ -0,0 +1,11 @@
+package pl.mibar.shipWars
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michalnawalany
+ * Date: 14/08/2013
+ * Time: 22:31
+ * To change this template use File | Settings | File Templates.
+ */
+class SomeMainClassTest {
+}
